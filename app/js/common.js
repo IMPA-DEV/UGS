d791dbd31d5a2d85a6cc81d16cb03cc4988c388f
MAXON-BY <maxon.pro2015@gmail.com>
2018-12-27 14:42:14 +0300

    готова главная страница V.1

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,8 @@
 $(function() {
 
-	// Custom JS
+    $('.choose-about').click(function () {
+        $('.choose-about').removeClass('active').next().slideUp();
+        $(this).toggleClass('active').next().slideDown();
+    });
 
 });
